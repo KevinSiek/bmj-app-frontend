59fa5164a8fb577bbfba6bb698d1f33c7d45c11f
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2025-03-06 18:23:33 +0700

    add login api

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,15 +1,17 @@
 <template>
-  <LoginDesktop :user="user" :login="login" />
+  <LoginDesktop :user="user" :login="login" :is-pass-show="isPassShow" :showHide="showHide" />
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
 import LoginDesktop from '@/components/LoginDesktop.vue'
 import { reactive, ref } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import { useModalStore } from '@/stores/modal'
 
-// const auth = useAuth()
+const auth = useAuthStore()
 const router = useRouter()
-// const infoStore = useInfoStore()
+const modalStore = useModalStore()
 
 const isLoading = ref(false)
 const isPassShow = ref(false)
@@ -22,13 +24,18 @@ const user = reactive({
 const login = async () => {
   isLoading.value = true
   try {
-    // await auth.login(user)
+    await auth.login(user)
     router.push('/menu')
   } catch (error) {
-    // infoStore.setInfo(true, 'Failed', 'Email or Password is incorrect')
+    modalStore.openMessageModal('failed', 'Email or Password is incorrect')
   }
   isLoading.value = false
 }
+
+const showHide = () => {
+  isPassShow.value = !isPassShow.value
+}
+
 </script>
 
 <style lang="scss" scoped></style>
