47202fb9c2d1c4457a53706a8bb2626c0415cebe
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2025-03-06 20:18:42 +0700

    fix auth

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,33 +1,34 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 import { ref } from 'vue'
+import { setToken, getToken, removeToken } from '@/utils/local-storage'
 
 export const useAuthStore = defineStore('auth-store', () => {
 	const user = ref(null)
 	const authenticated = ref(false)
 
-	const register = async (credentials) => {
-		try {
-			await axios.get('/api/sanctum/csrf-cookie')
-			await axios.post('/api/register', credentials)
-		} catch (error) {
-      user.value = null;
-      authenticated.value = false;
-			console.error('Error loading new arrivals:', error)
-			throw error
-		}
-	}
+	// const register = async (credentials) => {
+	// 	try {
+	// 		await axios.post('/api/register', credentials)
+	// 	} catch (error) {
+  //     user.value = null
+  //     authenticated.value = false
+	// 		console.error('Error loading new arrivals:', error)
+	// 		throw error
+	// 	}
+	// }
 
 	const login = async (credentials) => {
 		try {
-			await axios.get('/api/sanctum/csrf-cookie')
 			const response = await axios.post('/api/login', credentials)
-      user.value = response.data.user;
-      authenticated.value = true;
-      axios.defaults.headers.common.Authorization = `${response.data.token_type} ${response.data.access_token}`
+      await setToken('token-bmj', response.data.access_token, 24)
+      user.value = response.data.user
+      authenticated.value = true
+      axios.defaults.headers.common.Authorization = `Bearer ${getToken('token-bmj')}`
 		} catch (error) {
-			user.value = null;
-      authenticated.value = false;
+			user.value = null
+      authenticated.value = false
+      removeToken('token-bmj')
 			console.error('Error loading new arrivals:', error)
 			throw error
 		}
@@ -35,10 +36,10 @@ export const useAuthStore = defineStore('auth-store', () => {
 
 	const logout = async () => {
 		try {
-      await axios.get('/api/sanctum/csrf-cookie')
 			await axios.post('/api/logout')
 			user.value = null;
-      authenticated.value = false;
+      authenticated.value = false
+      removeToken('token-bmj')
       axios.defaults.headers.common.Authorization = null
 		} catch (error) {
 			console.error('Error loading new arrivals:', error)
@@ -48,19 +49,19 @@ export const useAuthStore = defineStore('auth-store', () => {
 
 	const getUser = async () => {
 		try {
+      axios.defaults.headers.common.Authorization = `Bearer ${getToken('token-bmj')}`
 			const response = await axios.get('/api/user')
-      user.value = response.data;
-      authenticated.value = true;
+      user.value = response.data
+      authenticated.value = true
 		} catch (error) {
-      user.value = null;
-      authenticated.value = false;
+      user.value = null
+      authenticated.value = false
 			console.error('Error loading new arrivals:', error)
 		}
 	}
 
 	const updateUser = async (credentials) => {
 		try {
-			await axios.get('/api/sanctum/csrf-cookie')
 			await axios.post('/api/profile/update', credentials)
 			await getUser()
 		} catch (error) {
@@ -71,7 +72,6 @@ export const useAuthStore = defineStore('auth-store', () => {
 
 	return{
 		user,
-		register,
 		login,
 		logout,
 		getUser,
