88a4a84cc8c78ab0da05ffea9e3fe314702c1347
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2025-03-07 13:05:05 +0700

    add delete employee

--- src/views/menu/EmployeeDetailPage.vue
+++ src/views/menu/EmployeeDetailPage.vue
@@ -14,7 +14,7 @@
       </div>
       <div class="input form-group col-12">
         <label for="role">Role</label><br>
-        <select class="form-select" id="role" v-model="employee.role">
+        <select class="form-select mt-2" id="role" v-model="employee.role">
           <option v-for="(role, index) in roles" :key="index" :value="role">
             {{ role }}
           </option>
@@ -22,32 +22,93 @@
       </div>
       <div class="input form-group col-6">
         <label for="password">Password</label><br>
-        <input type="password" class="form-control mt-2" v-model="employee.password" placeholder="Password">
+        <div class="input-group mt-2">
+          <input v-if="!isPassShow" type="password" class="input form-control" v-model="employee.password"
+            placeholder="Password">
+          <input v-else type="text" class="input form-control" v-model="employee.password" placeholder="Password">
+          <span class="input input-group-text">
+            <button type="button" @click="showHidePass()" class="showHideBtn">
+              <svg v-if="!isPassShow" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-eye" viewBox="0 0 16 16">
+                <path
+                  d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+              </svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-eye-slash" viewBox="0 0 16 16">
+                <path
+                  d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                <path
+                  d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                <path
+                  d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
+              </svg>
+            </button>
+          </span>
+        </div>
       </div>
       <div class="input form-group col-6">
         <label for="password">Confirm Password</label><br>
-        <input type="password" class="form-control mt-2" v-model="employee.password_confirmation"
-          placeholder="Retype Password">
+        <div class="input-group mt-2">
+          <input v-if="!isConfPassShow" type="password" class="input form-control"
+            v-model="employee.password_confirmation" placeholder="Retype Password">
+          <input v-else type="text" class="input form-control" v-model="employee.password_confirmation"
+            placeholder="Retype Password">
+          <span class="input input-group-text">
+            <button type="button" @click="showHideConfPass()" class="showHideBtn">
+              <svg v-if="!isConfPassShow" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-eye" viewBox="0 0 16 16">
+                <path
+                  d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+              </svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-eye-slash" viewBox="0 0 16 16">
+                <path
+                  d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                <path
+                  d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                <path
+                  d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
+              </svg>
+            </button>
+          </span>
+        </div>
       </div>
     </form>
   </div>
   <div class="button">
-    <button type="button" class="btn btn-danger" @click="resetPassword()">
-      Reset Password
-    </button>
-    <button type="submit" class="btn btn-update">
+    <div class="left">
+      <button type="button" class="btn btn-danger mx-3" @click="deleteEmployeeConfirmation">
+        Delete Employee
+      </button>
+      <button type="button" class="btn btn-secondary mx-3" @click="resetPassword">
+        Reset Password
+      </button>
+    </div>
+    <button type="submit" class="btn btn-update" @click="updateEmployeeConfirmation">
       Update Employee
     </button>
   </div>
 </template>
 
 <script setup>
+import { menuMapping } from '@/config'
 import { useEmployeeStore } from '@/stores/employee'
+import { useModalStore } from '@/stores/modal'
+import { validatePassword } from '@/utils/form-util'
 import { storeToRefs } from 'pinia'
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
+const router = useRouter()
+const route = useRoute()
+const modalStore = useModalStore()
 const employeeStore = useEmployeeStore()
 
 const { employee } = storeToRefs(employeeStore)
+const isPassShow = ref(false)
+const isConfPassShow = ref(false)
 
 const roles = [
   'Director',
@@ -57,8 +118,42 @@ const roles = [
   'Service'
 ]
 
-const resetPassword = () => {
+onMounted(() => {
+  employeeStore.getEmployee(route.params.id)
+})
 
+const resetPassword = () => {
+}
+
+const showHidePass = () => {
+  isPassShow.value = !isPassShow.value
+}
+const showHideConfPass = () => {
+  isConfPassShow.value = !isConfPassShow.value
+}
+
+const updateEmployee = async () => {
+  if (validatePassword(employee.value.password, employee.value.password_confirmation)) {
+    employeeStore.updateEmployee()
+  }
+  else {
+    throw new Error('Confirm Password is not Match')
+  }
+}
+const updateEmployeeConfirmation = () => {
+  modalStore.openConfirmationModal('Are you sure to Update this Employee ?', 'Update Employee Success', updateEmployee)
+}
+const deleteEmployee = async () => {
+  if (validatePassword(employee.value.password, employee.value.password_confirmation)) {
+    await employeeStore.deleteEmployee(employee.value.id)
+    router.push(menuMapping.employee.path)
+  }
+  else {
+    throw new Error('Confirm Password is not Match')
+  }
+}
+const deleteEmployeeConfirmation = () => {
+  modalStore.openConfirmationModal('Are you sure to Delete this Employee ?', 'Delete Employee Success', deleteEmployee)
 }
 
 </script>
@@ -78,6 +173,12 @@ $primary-color: black;
 
   .input {
     margin: 1% 0%;
+
+    .showHideBtn {
+      background-color: transparent;
+      border-color: transparent;
+      display: flex;
+    }
   }
 }
 
