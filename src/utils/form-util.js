deef7389d61f9de0dec815fbedd57079ca6ee7b6
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2025-05-01 18:54:07 +0700

    add formatCurrency

--- src/utils/form-util.js
+++ src/utils/form-util.js
@@ -3,6 +3,14 @@ const validatePassword = (password, confirmPassword) => {
   return false
 }
 
+const formatCurrency = (value) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR"
+  }).format(value)
+}
+
 export {
-  validatePassword
+  validatePassword,
+  formatCurrency
 }
