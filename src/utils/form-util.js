74b0b2f2b751ea63857aed48b153a88a17cfcb04
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2025-06-22 18:23:00 +0700

    add form util

--- src/utils/form-util.js
+++ src/utils/form-util.js
@@ -4,13 +4,39 @@ const validatePassword = (password, confirmPassword) => {
 }
 
 const formatCurrency = (value) => {
+  if(!value) return 0
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR"
   }).format(value)
 }
 
+const formatDate = (timestamp) => {
+  if(!timestamp) return null
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }).format(new Date(timestamp))
+}
+
+const formatDateAndTime = (timestamp) => {
+  if(!timestamp) return null
+  const options = {
+    year: 'numeric',
+    month: 'long', // or '2-digit'
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false // use 24-hour format
+  };
+  return new Date(timestamp).toLocaleString('en-US', options);
+}
+
 export {
   validatePassword,
-  formatCurrency
+  formatCurrency,
+  formatDate,
+  formatDateAndTime
 }
