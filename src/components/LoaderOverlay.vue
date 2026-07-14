53431309abed54075a1dac1a55202d7ac597beb7
Kevin Widjanarko <kevin.widjanarko@gdn-commerce.com>
2026-07-14 10:36:03 +0700

    add loader

---
+++ src/components/LoaderOverlay.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="loader-overlay">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/css/loader.scss';
+</style>
---
+++ src/components/LoaderOverlaySmall.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="loader-overlay-small">
+    <div class="loader-small"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/css/loader.scss';
+</style>
